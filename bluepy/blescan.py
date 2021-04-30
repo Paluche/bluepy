a9d129c9b37f5b77c3fa373eaea4d5f09a86fe1a
Hubert Lefevre <hubelefevre@gmail.com>
2021-04-30 20:45:29 +0200

    blescan: Make it pass pylint
    Documentation ignored.

--- bluepy/blescan.py
+++ bluepy/blescan.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import argparse
 import binascii
 import os
@@ -23,36 +24,39 @@ else:
 
 
 def dump_services(dev):
-    services = sorted(dev.services, key=lambda s: s.hndStart)
-    for s in services:
-        print ("\t%04x: %s" % (s.hndStart, s))
-        if s.hndStart == s.hndEnd:
+    services = sorted(dev.services, key=lambda service: service.hndStart)
+    for service in services:
+        print("\t%04x: %s" % (service.hndStart, service))
+        if service.hndStart == service.hndEnd:
             continue
-        chars = s.getCharacteristics()
-        for i, c in enumerate(chars):
-            props = c.propertiesToString()
-            h = c.getHandle()
+
+        chars = service.getCharacteristics()
+        for i, characteristic in enumerate(chars):
+            props = characteristic.propertiesToString()
+            handle = characteristic.getHandle()
             if 'READ' in props:
-                val = c.read()
-                if c.uuid == btle.AssignedNumbers.device_name:
+                val = characteristic.read()
+                if characteristic.uuid == btle.AssignedNumbers.device_name:
                     string = ANSI_CYAN + '\'' + \
                         val.decode('utf-8') + '\'' + ANSI_OFF
-                elif c.uuid == btle.AssignedNumbers.device_information:
+                elif characteristic.uuid == btle.AssignedNumbers.device_information:
                     string = repr(val)
                 else:
                     string = '<s' + binascii.b2a_hex(val).decode('utf-8') + '>'
             else:
                 string = ''
-            print ("\t%04x:    %-59s %-12s %s" % (h, c, props, string))
+            print("\t%04x:    %-59s %-12s %s" % (handle, characteristic, props, string))
 
             while True:
-                h += 1
-                if h > s.hndEnd or (i < len(chars) - 1 and h >= chars[i + 1].getHandle() - 1):
+                handle += 1
+                if (handle > service.hndEnd or
+                    (i < len(chars) - 1 and
+                     handle >= chars[i + 1].getHandle() - 1)):
                     break
                 try:
-                    val = dev.readCharacteristic(h)
+                    val = dev.readCharacteristic(handle)
                     print("\t%04x:     <%s>" %
-                          (h, binascii.b2a_hex(val).decode('utf-8')))
+                          (handle, binascii.b2a_hex(val).decode('utf-8')))
                 except btle.BTLEException:
                     break
 
@@ -78,62 +82,97 @@ class ScanPrint(btle.DefaultDelegate):
         if dev.rssi < self.opts.sensitivity:
             return
 
-        print ('    Device (%s): %s (%s), %d dBm %s' %
-               (status,
-                   ANSI_WHITE + dev.addr + ANSI_OFF,
-                   dev.addrType,
-                   dev.rssi,
-                   ('' if dev.connectable else '(not connectable)'))
-               )
+        print('    Device (%s): %s (%s), %d dBm %s' %
+              (status,
+                  ANSI_WHITE + dev.addr + ANSI_OFF,
+                  dev.addrType,
+                  dev.rssi,
+                  ('' if dev.connectable else '(not connectable)'))
+              )
+
         for (sdid, desc, val) in dev.getScanData():
             if sdid in [8, 9]:
-                print ('\t' + desc + ': \'' + ANSI_CYAN + val + ANSI_OFF + '\'')
+                print('\t' + desc + ': \'' + ANSI_CYAN + val + ANSI_OFF + '\'')
             else:
-                print ('\t' + desc + ': <' + val + '>')
+                print('\t' + desc + ': <' + val + '>')
         if not dev.scanData:
-            print ('\t(no data)')
-        print
+            print('\t(no data)')
+        print()
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--hci', action='store', type=int, default=0,
-                        help='Interface number for scan')
-    parser.add_argument('-t', '--timeout', action='store', type=int, default=4,
-                        help='Scan delay, 0 for continuous')
-    parser.add_argument('-s', '--sensitivity', action='store', type=int, default=-128,
-                        help='dBm value for filtering far devices')
-    parser.add_argument('-d', '--discover', action='store_true',
-                        help='Connect and discover service to scanned devices')
-    parser.add_argument('-a', '--all', action='store_true',
-                        help='Display duplicate adv responses, by default show new + updated')
-    parser.add_argument('-n', '--new', action='store_true',
-                        help='Display only new adv responses, by default show new + updated')
-    parser.add_argument('-v', '--verbose', action='store_true',
-                        help='Increase output verbosity')
+    parser.add_argument(
+        '-i',
+        '--hci',
+        action='store',
+        type=int,
+        default=0,
+        help='Interface number for scan'
+    )
+    parser.add_argument(
+        '-t',
+        '--timeout',
+        action='store',
+        type=int,
+        default=4,
+        help='Scan delay, 0 for continuous'
+    )
+    parser.add_argument(
+        '-s',
+        '--sensitivity',
+        action='store',
+        type=int,
+        default=-128,
+        help='dBm value for filtering far devices'
+    )
+    parser.add_argument(
+        '-d',
+        '--discover',
+        action='store_true',
+        help='Connect and discover service to scanned devices'
+    )
+    parser.add_argument(
+        '-a',
+        '--all',
+        action='store_true',
+        help='Display duplicate adv responses, by default show new + updated'
+    )
+    parser.add_argument(
+        '-n',
+        '--new',
+        action='store_true',
+        help='Display only new adv responses, by default show new + updated'
+    )
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='store_true',
+        help='Increase output verbosity'
+    )
+
     arg = parser.parse_args(sys.argv[1:])
 
     btle.Debugging = arg.verbose
 
     scanner = btle.Scanner(arg.hci).withDelegate(ScanPrint(arg))
 
-    print (ANSI_RED + "Scanning for devices..." + ANSI_OFF)
+    print(ANSI_RED + "Scanning for devices..." + ANSI_OFF)
     devices = scanner.scan(arg.timeout)
 
     if arg.discover:
-        print (ANSI_RED + "Discovering services..." + ANSI_OFF)
+        print(ANSI_RED + "Discovering services..." + ANSI_OFF)
 
-        for d in devices:
-            if not d.connectable or d.rssi < arg.sensitivity:
-
+        for device in devices:
+            if not device.connectable or device.rssi < arg.sensitivity:
                 continue
 
-            print ("    Connecting to", ANSI_WHITE + d.addr + ANSI_OFF + ":")
+            print("    Connecting to", ANSI_WHITE + device.addr + ANSI_OFF + ":")
 
-            dev = btle.Peripheral(d)
+            dev = btle.Peripheral(device)
             dump_services(dev)
             dev.disconnect()
-            print
+            print()
 
 if __name__ == "__main__":
     main()
